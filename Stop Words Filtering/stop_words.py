60652fe49c5566f66c6844918c99806e71f77dc9
Aalind Singh <singh.aalind@gmail.com>
2016-12-13 20:16:32 +0530

    Stop Words Filtering

---
+++ Stop Words Filtering/stop_words.py
@@ -0,0 +1,22 @@
+# Stop Words
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+Example_sentence = "Hi there developer, what's up ? Machine Learning is awesome. Do not puke on your screen."
+
+stop_words = set(stopwords.words("english"))
+
+words = word_tokenize(Example_sentence)
+
+filtered_sentence = []
+
+for w in words:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+        
+# basically doing the same thing as the above loop but now in one line.
+filtered_sent = [w for w in words if not w in stop_words]
+        
+print(filtered_sentence)
+print(filtered_sent)
