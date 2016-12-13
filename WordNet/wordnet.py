22bb43eee0f6cd04d01dcd3ecc112202f5c3a1f0
Aalind Singh <singh.aalind@gmail.com>
2016-12-14 03:42:10 +0530

    WordNet

---
+++ WordNet/wordnet.py
@@ -0,0 +1,45 @@
+# WordNet 
+from nltk.corpus import wordnet
+
+syns = wordnet.synsets("programs")
+
+# synonym set
+print(syns[0].name())
+
+# just the word
+print(syns[0].lemmas()[0].name())
+
+# definition
+print(syns[0].definition())
+
+# examples
+print(syns[0].examples())
+
+synonyms = []
+antonyms = []
+
+for syn in wordnet.synsets("good"):
+    for l in syn.lemmas():
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name())
+            
+print(set(synonyms))
+print(set(antonyms))
+
+# Similarity Comparison
+
+w1 = wordnet.synset("ship.n.01")
+w2 = wordnet.synset("boat.n.01")
+
+print(w1.wup_similarity(w2))
+
+w1 = wordnet.synset("ship.n.01")
+w2 = wordnet.synset("car.n.01")
+
+print(w1.wup_similarity(w2))
+
+w1 = wordnet.synset("ship.n.01")
+w2 = wordnet.synset("cactus.n.01")
+
+print(w1.wup_similarity(w2))
