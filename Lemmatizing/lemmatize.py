6bf2a28f6dd90791d146b0fa177340298b214c53
Aalind Singh <singh.aalind@gmail.com>
2016-12-14 00:03:50 +0530

    Lemmatizer

---
+++ Lemmatizing/lemmatize.py
@@ -0,0 +1,20 @@
+# Lemmatizing
+# Similar to stemming but gives meaningful words which is not always the case with stemming.
+
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+print(lemmatizer.lemmatize("cats"))
+print(lemmatizer.lemmatize("cacti"))
+print(lemmatizer.lemmatize("geese"))
+print(lemmatizer.lemmatize("rocks"))
+print(lemmatizer.lemmatize("python"))
+
+# alongwith part of speech tagging.
+# lemmatize takes a part of speech parameter, "pos." If not supplied, the default is "noun."
+
+print(lemmatizer.lemmatize("better", pos="a"))
+print(lemmatizer.lemmatize("best", pos="a"))
+print(lemmatizer.lemmatize("run"))
+print(lemmatizer.lemmatize("run",'v'))
